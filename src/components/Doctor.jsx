51364c66d5b01324c622fbdb5990bc7ce015bd9e
El-natty <owaroruno@gmail.com>
2025-09-11 18:00:55 +0100

    changers

---
+++ src/components/Doctor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Doctors = () => {
+  return (
+    <div>
+      <h1>Doctors Component</h1>
+      <p>List of doctors will be here.</p>
+    </div>
+  )
+}
+
+export default Doctors
